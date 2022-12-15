f92c3a14de0cfade635b4f45278281337c60fd97
setouchi-h <setouchi.kazuki@gmail.com>
2022-12-16 01:11:38 +0900

    deposit

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,6 +2,7 @@ const networkConfig = {
     31337: {
         name: "localhost",
         wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        lendingPoolAddressesProvider: "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
     }
 }
 
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,17 +1,50 @@
-const { getWeth } = require("../scripts/getWeth")
-const { getNamedAccounts } = require("hardhat")
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
 
 async function main() {
     // the protocal treats everything as an ERC20 token
     await getWeth()
     const { deployer } = await getNamedAccounts()
+    // Lending Pool
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool Address ${lendingPool.address}`)
+
+    // deposit
+    const wethTokenAddress = networkConfig[network.config.chainId].wethToken
+    // approve
+    await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited!")
+
 }
 
-async function getLendingPoor() {}
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        networkConfig[network.config.chainId].lendingPoolAddressesProvider,
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
+}
+
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt(
+        "IERC20",
+        erc20Address,
+        account
+    )
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved!")
+}
 
 main()
     .then(() => process.exit(0))
-    .catch(error => {
+    .catch((error) => {
         console.error(error)
         process.exit(1)
     })
